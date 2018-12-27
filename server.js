6c489a12ec62593497316bf0f1b777dcd44d50a6
Zafar Ali <zafar.a.ali2@gmail.com>
2018-12-27 12:33:34 -0500

    Username can be created and used
    - Username created in namesWrapper and passed onto server to be used in mainWrapper

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
   app = express(),
   server = require('http').createServer(app),
   io = require('socket.io').listen(server);
-
+  usernames = [];
 server.listen(process.env.PORT || 3000);
 console.log("Server Running");
 
@@ -13,8 +13,43 @@ app.get('/', function(req, res){
 io.sockets.on('connection', function(socket){
   console.log('Socket Connected');
 
+  socket.on('new user', function(data, callback){
+    if(usernames.indexOf(data)!=-1){
+      callback(false);
+
+    }else{
+      callback(true);
+      socket.username=data;
+      usernames.push(socket.username);
+      updateUsernames();
+    }
+  });
+
+  //Update usernames
+  function updateUsernames(){
+    io.sockets.emit('usernames', usernames);
+
+  }
+
+
   //Send the message
   socket.on('send message', function(data){
     io.sockets.emit('new message', {msg: data});
   });
+
+
+//Disconnect
+socket.on('disconnect', function(data){
+
+  if(!socket.username){
+    return;
+  }
+  usernames.splice(usernames.indexOf(socket.username), 1);
+  updateUsernames();
+
+
+});
+
+
+
 });
